f27baf042f2c6a92c5e32140e91bb168c4ba611a
manny2trill <manuelr42787@gmail.com>
2022-03-21 01:55:53 -0500

    started working out some code to start working w/

--- name_generator/pseudonyms.py
+++ name_generator/pseudonyms.py
@@ -1,2 +1,27 @@
-import system, random
+import sys, random
 
+# print statement for the user to know what the program does
+print("This is a program that generates a random hacker name for you.")
+
+# created some  first names
+first = ('Baby','Big','Young',)
+# created some last names
+last = ('Worm', 'Morty', 'Chapo',)
+
+# created a while loop for the user to keep generating names
+while True :
+    firstName = random.choice(first)
+    lastName = random.choice(last)
+
+
+print('\n\n')
+print("{} {}".format(firstName, lastName), file.sys.stdeer)
+print('\n\n')
+
+
+try_again = input('\n\n Would you like to try again? (y/n): ')
+
+if try_again.lower() == 'n':
+#break
+
+    input('\n\n Press Enter to exit.')    
