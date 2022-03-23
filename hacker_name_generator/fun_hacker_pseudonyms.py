54a44aa4b6fe7d3b9e58c1e0e6baa4d95a97be02
manny2trill <manuelr42787@gmail.com>
2022-03-23 00:17:23 -0500

    added comments to the code

--- hacker_name_generator/fun_hacker_pseudonyms.py
+++ hacker_name_generator/fun_hacker_pseudonyms.py
@@ -1,7 +1,7 @@
 import sys
 import random
 
-
+# created a function to generate a random name
 def main():
 
 # print statement for the user to know what the program does
@@ -19,12 +19,13 @@ def main():
         print('\n')
         print("{} {}".format(first_name, last_name), file=sys.stderr)
         print('\n')
-    
+        # ask the user if they want to try again
         try_again = input('\n Would you like to try again? (y/n): ')
-
+        # if the user wants to try again, the program will continue
+        # if the user does not want to try again, the program will lead user to exit
         if try_again.lower() == 'n':
             break
-
+    # asks user to press enter to exit
     input('\n\n Press Enter to exit.')    
 
 if __name__=="__main__":
